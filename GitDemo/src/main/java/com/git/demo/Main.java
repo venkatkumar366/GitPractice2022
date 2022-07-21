0faaee2ff816885381e90f727755169fdef62393
venkatkumar366 <49677825+venkatkumar366@users.noreply.github.com>
2022-07-21 09:50:39 +0530

    First Commit

---
+++ GitDemo/src/main/java/com/git/demo/Main.java
@@ -0,0 +1,7 @@
+package com.git.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("demo for adding in git");
+    }
+}
---
+++ GitDemo/src/test/java/com/git/demotest/MainTest.java
@@ -0,0 +1,7 @@
+package com.git.demotest;
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("the test class");
+    }
+}
