f87fa205a8e1e9519b9904d2a98dc40ccbcff7c3
venkatkumar366 <49677825+venkatkumar366@users.noreply.github.com>
2022-07-21 10:06:01 +0530

    Added the multiple checklist to the default like java 8 java 11 etc

--- GitDemo/src/main/java/com/git/demo/Main.java
+++ GitDemo/src/main/java/com/git/demo/Main.java
@@ -3,5 +3,12 @@ package com.git.demo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("demo for adding in git");
+
+        demo();
+    }
+
+    private static void demo() {
+        System.out.println("Add");
+        //change List is nothing but stash - have added multiple changelist like DS JAVA8 Java 11
     }
 }
